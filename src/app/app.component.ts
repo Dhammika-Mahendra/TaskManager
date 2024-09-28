59cdd6076c19ca8d3ea0c77bd30c89bc609a8e4f
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-09-28 23:43:50 +0530

    dynamic navbar by role

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { StorageService } from './auth/services/storage/storage.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatToolbarModule,RouterLink,MatButtonModule],
+  imports: [RouterOutlet,MatToolbarModule,RouterLink,MatButtonModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'TaskManager';
+
+  isEmployeeLoggedIn: boolean=StorageService.isEmployeeLoggedIn();
+  isAdminLoggedIn: boolean = StorageService.isAdminLoggedIn();
+
+  constructor(private router: Router) { }
+  
+  ngOnInit() {
+      this.router.events.subscribe (event => {
+      this.isEmployeeLoggedIn=StorageService.isEmployeeLoggedIn();
+      this.isAdminLoggedIn=StorageService.isAdminLoggedIn();
+    })
+  }
+
+  logout(){
+    StorageService.logout();
+    this.router.navigate(["/login"])
+  }
 }
